2af642ca6ec2514473ae196725ffa8282913543a
Isha Jain <ishajainpdx@gmail.com>
2025-12-24 22:38:43 -0500

    Testing connection between Github + Cloudflare

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
 						.
 					</li>
 					<li className="tracking-[-.01em]">Save and see your changes instantly.</li>
+					<li className="tracking-[-.01em]">Deploy to Cloudflare!</li>
 				</ol>
 
 				<div className="flex gap-4 items-center flex-col sm:flex-row">
